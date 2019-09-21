1c527339a4ee96e7ef06252631ee0dd39ea83505
Juliana <jfn1991@gmail.com>
2019-09-21 14:09:35 -0300

    Initial

---
+++ TesteCores.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+/* import logo from './logo500.png'; */
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+   {/*  <img src={logo} alt="logo" /> */}
+    <div className="md navBar"></div>
+    <div className="md menu"></div>
+    <div className="md corpo"></div>
+    <div className="footer"></div>
+   </>
+  );
+}
+
+export default App;
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import NavBar from './components/Navbar'
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <h1>Olá</h1>
+  );
+}
+
+export default App;
