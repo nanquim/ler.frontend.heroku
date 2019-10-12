be70cf7580d0dd64129c282cdcbacf0ce687dd26
Juliana <jfn1991@gmail.com>
2019-10-12 08:21:44 -0300

    config

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Menu from './componentes/Menu'
+/* import FormAssociar from './componentes/Formularios/Associar'
+import FormContato from './componentes/Formularios/Contato'
+import Sobre from './componentes/Paginas/Sobre' */
+import Blog from './componentes/Blog'
+
+export default function App() {
+  return (
+    <Router>
+      <Menu />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Blog} />
+         {/*  <Route path="/sobre" component={Sobre} />
+          <Route path="/associar" component={FormAssociar} />
+          <Route path="/contato" component={FormContato} /> */}
+        </Switch>
+      </main>
+    </Router>
+  );
+}
