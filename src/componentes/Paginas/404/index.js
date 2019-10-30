cf2eaa9480081b2f9c3371ae2385d6f1a6543eab
Juliana <jfn1991@gmail.com>
2019-10-30 16:34:24 -0300

    404

---
+++ src/componentes/Paginas/404/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.css'
+
+const NaoEncontrado = () => {
+ 
+  return (
+     <div className="nao-encontrada">
+        <div>404</div>
+        <div>Página não encontrada</div>
+     </div>     
+  )
+}
+
+export default NaoEncontrado;
