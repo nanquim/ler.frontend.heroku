b9eae943c354e8842adb7d608ebf785f239b2184
Juliana <jfn1991@gmail.com>
2019-10-30 16:33:30 -0300

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Menu from './componentes/Menu'
-import FormAssociar from './componentes/Formularios/Associar'
-import FormContato from './componentes/Formularios/Contato'
-import Sobre from './componentes/Paginas/Sobre'
-import Blog from './componentes/Blog'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Menu from './componentes/Menu';
+import FormAssociar from './componentes/Formularios/Associar';
+import FormContato from './componentes/Formularios/Contato';
+import Sobre from './componentes/Paginas/Sobre';
+import Blog from './componentes/Blog';
+import Agenda from './componentes/Agenda';
+import NaoEncontrado from './componentes/Paginas/404'
+import './App.css';
 
-/* 
- TODO reutilizar forms, esta horrivel...
-*/
-
- export default function App() {
+const App = () => {
   return (
     <Router>
       <Menu />
@@ -19,10 +17,14 @@ import './App.css'
         <Switch>
           <Route exact path="/" component={Blog} />
           <Route path="/sobre" component={Sobre} />
+          <Route path="/agenda" component={Agenda} />
           <Route path="/associar" component={FormAssociar} />
           <Route path="/contato" component={FormContato} />
+          <Route component={NaoEncontrado} />
         </Switch>
       </main>
     </Router>
   );
 }
+
+export default App;
